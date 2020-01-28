7b2f0832c39aae4cff569286844145ac646855bd
Liu <59314355+zkzk5214@users.noreply.github.com>
2020-01-28 18:06:55 -0500

    Create forw_fc.m

---
+++ 1/forw_fc.m
@@ -0,0 +1,14 @@
+function [y] = forw_fc(x,w,b)
+% Input:       x An matrix of size M * N
+%              w An matrix of size M * N
+%              b A scalar bias value 
+% Output:      An scalar value
+
+sum = 0;
+for X = 1:size(x,1)
+    for Y = 1:size(x,2)
+        sum = sum + x(X,Y)+w(X,Y);
+    end
+end
+y = sum+b;
+end
