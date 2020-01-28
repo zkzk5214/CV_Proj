c12e57b85f9cfad7f9955ac204dc500e4df7e2c6
Liu <59314355+zkzk5214@users.noreply.github.com>
2020-01-28 17:45:42 -0500

    Update forw_relu.m

--- 1/forw_relu.m
+++ 1/forw_relu.m
@@ -1,6 +1,6 @@
 function [y] = forw_relu(x)
-% Input:       An array of size N * M * D
-% Output:      An array of the same size
+% Input:       An matrix of size M * N 
+% Output:      An matrix of the same size
 
 y = max(x,0);
 end
