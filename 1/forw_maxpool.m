fbb42710bba7d81d7a8a3243a5e711282ca242c1
Liu <59314355+zkzk5214@users.noreply.github.com>
2020-01-28 17:46:12 -0500

    Update forw_maxpool.m

--- 1/forw_maxpool.m
+++ 1/forw_maxpool.m
@@ -1,6 +1,6 @@
 function [y] = forw_maxpool(x)
-% Input:       An matrix of size 2N * 2M
-% Output:      An matrix of the size N * M
+% Input:       An matrix of size 2M * 2N
+% Output:      An matrix of size M * N
 
 a = max(x(1:2:end,1:2:end,:),x(2:2:end,1:2:end,:));
 b = max(x(1:2:end,2:2:end,:),x(2:2:end,2:2:end,:));
