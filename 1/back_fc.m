2f49adc7fd7c8ec91b3f0968e50488a6331e47d1
Liu <59314355+zkzk5214@users.noreply.github.com>
2020-01-28 23:27:15 -0500

    Create back_fc.m

---
+++ 1/back_fc.m
@@ -0,0 +1,13 @@
+function [dzdx,dzdw,dzdb] = back_fc(x,w,dzdy)
+% Input:       x An matrix of size M * N
+%              w An matrix of size M * N 
+%              b a scalar bias value
+%              y a scalar value (output from forward pass) 
+%              dzdy a scalar value dx/dy
+% Output:      dzdx an matrix of size M * N 
+%              dzdw an matrix of size M * N  
+%              dzdb a value 
+
+dzdw = dzdy .* x;
+dzdx = dzdy .* w;
+dzdb = sum(dzdy,1);
