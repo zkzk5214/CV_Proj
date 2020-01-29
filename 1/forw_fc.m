e99c4af9b9bb40b6855b9fcec1db9a3cdac8403d
Liu <59314355+zkzk5214@users.noreply.github.com>
2020-01-29 16:10:28 -0500

    Update forw_fc.m

--- 1/forw_fc.m
+++ 1/forw_fc.m
@@ -7,7 +7,7 @@ function [y] = forw_fc(x,w,b)
 sum = 0;
 for X = 1:size(x,1)
     for Y = 1:size(x,2)
-        sum = sum + x(X,Y) * w(X,Y);
+        sum = sum + x(X,Y) .* w(X,Y);
     end
 end
 y = sum+b;
