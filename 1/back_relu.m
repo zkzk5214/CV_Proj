3941db5a9735a1179dbc268fc9e44c47a5b65cff
Liu <59314355+zkzk5214@users.noreply.github.com>
2020-01-28 22:09:28 -0500

    Create back_relu.m

---
+++ 1/back_relu.m
@@ -0,0 +1,20 @@
+function [dzdx] = back_relu(x,dzdy)
+% Input:       x An matrix of size M * N
+%              y An matrix of size M * N
+%              dzdy An matrix of size M * N 
+% Output:      An matrix of size M * N
+
+[m,n]=size(dzdy);
+dydx = zeros(m,n);
+dzdx = zeros(m,n);
+
+for X = 1:m
+    for Y = 1:n     
+        if x(X,Y)>0
+            dydx(X,Y) =1;
+        else
+            dydx(X,Y) =0;
+        end
+        dzdx(X,Y) = dzdy(X,Y) * dydx(X,Y);
+    end
+end
