8b815404bbea3432e847d941ab5ab9d1c781182c
Liu <59314355+zkzk5214@users.noreply.github.com>
2020-01-29 11:45:01 -0500

    Update back_softmax.m

--- 1/back_softmax.m
+++ 1/back_softmax.m
@@ -4,16 +4,6 @@ function [dzdx] = back_softmax(x,y,dzdy)
 %              dzdy An vector of size M * 1
 % Output:      dzdx An vector of size M * 1
 
-[m,n] = size(y);
-dzdx = zeros(m,1);
-
-for i =1 : m
-%     for j = 1 : n
-%          if i == j
-             dzdx(i) = y(i) * (1-y(i));
-%           else
-%               dzdx(i) = -y(i)*y(j);
-%          end
-%  	 end
- end
+der = -y'*y + diag(y,0);
+dzdx = der * dzdy;
 end
