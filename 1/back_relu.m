bb6f35fbffcbfcf43780d59c77dcbaac6652c65a
Liu <59314355+zkzk5214@users.noreply.github.com>
2020-01-30 20:26:42 -0500

    Update back_relu.m

--- 1/back_relu.m
+++ 1/back_relu.m
@@ -4,10 +4,12 @@ function [dzdx] = back_relu(x,y,dzdy)
 %              dzdy An matrix of size M * N 
 % Output:      dzdx An matrix of size M * N
 
-[m,n]=size(dzdy);
-dydx = zeros(m,n);
-dzdx = zeros(m,n);
+[m,n]=size(dzdy);   % Array dzdy size M * N
+dydx = zeros(m,n);  % Set the fixed size to dydx  
+dzdx = zeros(m,n);  % Set the fixed size to dzdx
 
+% The derivative of ReLU(dydx) is 
+% f'(x)=1, if x>0 ; 0, otherwise
 for X = 1:m
     for Y = 1:n     
         if x(X,Y)>0
@@ -15,6 +17,6 @@ for X = 1:m
         else
             dydx(X,Y) =0;
         end
-        dzdx(X,Y) = dzdy(X,Y) * dydx(X,Y);
+        dzdx(X,Y) = dzdy(X,Y) * dydx(X,Y);  % Chain Rule
     end
 end
