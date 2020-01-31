705b745b58ea1ccf47305e39535849e67dc83dd0
Liu <59314355+zkzk5214@users.noreply.github.com>
2020-01-30 21:10:24 -0500

    Update forw_maxpool.m

--- 1/forw_maxpool.m
+++ 1/forw_maxpool.m
@@ -1,8 +1,11 @@
 function [y] = forw_maxpool(x)
-% Input:       An matrix of size 2M * 2N
-% Output:      An matrix of size M * N
+% Input:       x An matrix of size 2M * 2N
+% Output:      y An matrix of size M * N
 
+% Find the bigger number between NW NE 
 a = max(x(1:2:end,1:2:end,:),x(2:2:end,1:2:end,:));
+% Find the bigger number between SW SE
 b = max(x(1:2:end,2:2:end,:),x(2:2:end,2:2:end,:));
+% Find the most biggest number between NW NE SW SE
 y = max(a,b);
 end
