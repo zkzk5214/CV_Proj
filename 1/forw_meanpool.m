ec07f2730cccc93d29d871dc7905a4f25d24b490
Liu <59314355+zkzk5214@users.noreply.github.com>
2020-01-30 21:09:36 -0500

    Update forw_meanpool.m

--- 1/forw_meanpool.m
+++ 1/forw_meanpool.m
@@ -1,8 +1,11 @@
 function [y] = forw_meanpool(x)
-% Input:       An matrix of size 2M * 2N
-% Output:      An matrix of size M * N
+% Input:       x An matrix of size 2M * 2N
+% Output:      y An matrix of size M * N
 
+% Find the mean between NW NE 
 a = x(1:2:end,1:2:end,:)+x(2:2:end,1:2:end,:);
+% Find the mean between SW SE
 b = x(1:2:end,2:2:end,:)+x(2:2:end,2:2:end,:);
+% Find the mean between NW NE SW SE
 y = (a+b)/4;
 end
