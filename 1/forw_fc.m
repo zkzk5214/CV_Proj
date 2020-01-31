be042780b589e82941c401bc262eac58eefbe0b5
Liu <59314355+zkzk5214@users.noreply.github.com>
2020-01-30 21:09:12 -0500

    Update forw_fc.m

--- 1/forw_fc.m
+++ 1/forw_fc.m
@@ -2,11 +2,14 @@ function [y] = forw_fc(x,w,b)
 % Input:       x An matrix of size M * N
 %              w An matrix of size M * N
 %              b A scalar bias value 
-% Output:      An scalar value
+% Output:      y An scalar value
 
 sum = 0;
+
+% y = summation(wij*xij) + b
 for X = 1:size(x,1)
     for Y = 1:size(x,2)
+        
         sum = sum + x(X,Y) .* w(X,Y);
     end
 end
